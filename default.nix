fcab27ffc21b2394c5fa7829246eaebead864d86
Michal Sojka <michal.sojka@cvut.cz>
2020-12-07 14:06:20 +0100

    Use later OpenCV version with few USAC fixes

--- default.nix
+++ default.nix
@@ -8,12 +8,12 @@ callPackage ./thermocam.nix {
   wic_sdk = wic_sdk;
   ebus_sdk = ebus_sdk;
   opencv = (opencv4.override { enableGtk3 = true; enableFfmpeg = true; }).overrideAttrs(oldAttrs: rec {
-    version = "d5dce632544f333306b2bf105d57a59c52054955";
+    version = "d986cc4861b978415fc20c3a0dc6f16ff9d0bcdf";
     src = fetchFromGitHub {
       owner  = "opencv";
       repo   = "opencv";
       rev    = version;
-      sha256 = "1z4kgy9nvg0x50rivzf0farr878vjl2s8n6hkxzkziq0xqlw2l3x";
+      sha256 = "1yaw4gz4zn4bcjhyv8knl490zvmy2pylnkz3pmvmc700pgwgy79a";
     };
   });
 }
