10c2fc4718f952ec68567235a79302ad1d6f8e42
Michal Sojka <michal.sojka@cvut.cz>
2021-02-16 14:44:35 +0100

    nix: Do not hardcode WIC_SDK location

--- wic_sdk.nix
+++ wic_sdk.nix
@@ -4,10 +4,18 @@ with pkgs;
 stdenv.mkDerivation rec {
   pname = "wic_sdk";
   version = "1.1.0";
-  src = ./WIC_SDK-Linux_Ubuntu_16.04_64b-1.1.0.run;
+  src = requireFile rec {
+    name = "WIC_SDK-Linux_Ubuntu_16.04_64b-1.1.0.run";
+    sha256 = "b97103de50ad81e3e664bd6b9f6090751f729e812630821e380a251c3108c2cc";
+    message = ''
+        Unfortunately, we cannot download file ${name} automatically.
+        Please add it to the Nix store manually using:
+          nix-store --add-fixed sha256 ${name}
+      '';
+  };
   outputs = [ "out" "ebus_sdk" ];
   buildCommand = ''
-    $src --target tmp --noexec
+    sh $src --target tmp --noexec
     cd tmp
 
     mkdir -p $out
