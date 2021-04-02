5cf082682e6a6d5898d698a539beca2342cdd212
Michal Sojka <michal.sojka@cvut.cz>
2021-04-02 03:25:14 +0200

    Initialize heat source images as 1x1 white pixel
    Otherwise, we crow print a reports a lot of unhandled exceptions.

--- webserver.hpp
+++ webserver.hpp
@@ -11,8 +11,8 @@ class Webserver
 private:
     std::mutex lock;
     cv::Mat img;
-    cv::Mat laplacian_img;
-    cv::Mat hs_img;
+    cv::Mat laplacian_img = {Size(1, 1), CV_8U, 255};
+    cv::Mat hs_img = {Size(1, 1), CV_8U, 255};
     std::vector<poi> POI;
     std::vector<poi> heat_sources;
     std::vector<std::pair<std::string,double>> cameraComponentTemps;
