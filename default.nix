f6e8a0db2b9d98ccbc5f55275c61185ef63224ed
Michal Sojka <michal.sojka@cvut.cz>
2021-04-09 10:44:50 +0200

    nix: Move opencv expression to a separate file
    This will allow easier testing of opencv compilation (e.g. in
    nix-shell).

--- default.nix
+++ default.nix
@@ -8,16 +8,8 @@ let
   libcurl = callPackage ./libcurl-gnutls-stub.nix {};
   wic_sdk = callPackage ./wic_sdk.nix { inherit libjpeg libcurl; };
   ebus_sdk = wic_sdk.ebus_sdk;
+  opencv = callPackage ./opencv.nix {};
 in
 callPackage ./thermocam.nix {
-  inherit wic_sdk ebus_sdk debug;
-  opencv = (opencv4.override { enableGtk3 = true; enableFfmpeg = true; }).overrideAttrs(oldAttrs: rec {
-    version = "d986cc4861b978415fc20c3a0dc6f16ff9d0bcdf";
-    src = fetchFromGitHub {
-      owner  = "opencv";
-      repo   = "opencv";
-      rev    = version;
-      sha256 = "1yaw4gz4zn4bcjhyv8knl490zvmy2pylnkz3pmvmc700pgwgy79a";
-    };
-  });
+  inherit wic_sdk ebus_sdk debug opencv;
 }
