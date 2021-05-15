CTU-IIG/thermocam-pcb

commit 99c2f8887950df977085505be35270cc55847d98

Fix POI editing

thermo_img.cpp
@@ -237,6 +237,7 @@ void thermo_img::track(const thermo_img &ref, tracking track)
 {
     switch (track) {
     case tracking::off:
+        poi = ref.poi; // just copy to reference points
         break;
     case tracking::sync:
         updateKpDesc();

thermocam-pcb.cpp
@@ -359,7 +359,7 @@ int main(int argc, char **argv)
     processStream(is, ref, curr, args);
 
     if (!args.poi_export_path.empty())
-        curr.write_poi_json(args.poi_export_path, true);
+        ref.write_poi_json(args.poi_export_path, true);
 
     if (webserver)
         webserver->terminate();

commit e03cae9312c41f4d81fa4db7703a1bab09e25add

Fix --track-points=once
Commit 99c2f88 ("Fix POI editing", 2021-05-13) broke point tracking
mode called "once". The tracked points position were immediately
rewritten by reference image points. We fix this by introducing new
internal tracking mode, called "copy", which is only used during POI
editing (with -e CLI option).

thermo_img.cpp
@@ -237,6 +237,8 @@ void thermo_img::track(const thermo_img &ref, tracking track)
 {
     switch (track) {
     case tracking::off:
+        break;
+    case tracking::copy:
         poi = ref.poi; // just copy to reference points
         break;
     case tracking::sync:

thermo_img.hpp
@@ -37,7 +37,7 @@ enum draw_mode { FULL, TEMP, NUM };
 
 struct thermo_img {
 public:
-    enum class tracking { off, sync, async, finish };
+    enum class tracking { off, copy, sync, async, finish };
     struct webimg {
         std::string name;
         std::string title;

thermocam-pcb.cpp
@@ -234,7 +234,7 @@ void processStream(img_stream &is, thermo_img &ref, thermo_img &curr, cmd_argume
 
     switch (args.tracking) {
     case cmd_arguments::tracking::off:
-        track = thermo_img::tracking::off;
+        track = args.enter_poi ? thermo_img::tracking::copy : thermo_img::tracking::off;
         break;
     case cmd_arguments::tracking::on:
         track = thermo_img::tracking::sync;