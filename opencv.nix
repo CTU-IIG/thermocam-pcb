5eaed9e70ecb74827b7a29b935f5edc7505ee04f
Michal Sojka <michal.sojka@cvut.cz>
2021-11-29 20:04:40 +0100

    nix: Upgrade nixpkgs and use opencv 4.5.4
    We no longer need to use (then) unreleased version.

--- opencv.nix
+++ opencv.nix
@@ -2,12 +2,4 @@
 , pkgs ? import sources.nixpkgs { }
 }:
 with pkgs;
-(opencv4.override { enableGtk3 = true; enableFfmpeg = true; }).overrideAttrs(oldAttrs: rec {
-    version = "d986cc4861b978415fc20c3a0dc6f16ff9d0bcdf";
-    src = fetchFromGitHub {
-      owner  = "opencv";
-      repo   = "opencv";
-      rev    = version;
-      sha256 = "1yaw4gz4zn4bcjhyv8knl490zvmy2pylnkz3pmvmc700pgwgy79a";
-    };
-})
+opencv4.override { enableGtk3 = true; enableFfmpeg = true; }
